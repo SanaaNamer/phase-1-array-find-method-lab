234523817635bb17dba9d9927cc70ed6e185ce72
san <sansamaa0@gmail.com>
2023-03-04 00:39:29 +0300

    h.w

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const record = [
+    {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+function superbowlWin(record) {
+        const w = record.find(record => record.result === 'W');
+      
+        return w ? w.year : undefined;
+}; 
+
+superbowlWin()
